d86166fb08958c9ddebfdc6c6003c515ae4e510c
MenSelder <yakushev_S.A@mail.ru>
2021-07-21 21:24:00 +0400

    sw change

--- testR/app.js
+++ testR/app.js
@@ -1,13 +1,28 @@
+// v1 - minin
 
-  window.addEventListener('load', async () => {
-    if ("serviceWorker" in navigator){
-        try{
-            const reg = await navigator.serviceWorker.register('/sw.js');
-            console.log("success reg", reg);
-        }
-        catch(e){
-            console.log("Sw reg error", e);
-        }
+  // window.addEventListener('load', async () => {
+  //   if ("serviceWorker" in navigator){
+  //       try{
+  //           const reg = await navigator.serviceWorker.register('/sw.js');
+  //           console.log("success reg", reg);
+  //       }
+  //       catch(e){
+  //           console.log("Sw reg error", e);
+  //       }
+  //   }
+  // })
+
+  // v2
+
+  window.addEventListener('load', () => {
+    if ('serviceWorker' in navigator){
+      navigator.serviceWorker.register('sw.js')
+        .then(registration => {
+          console.log('SW registred', registration);
+        })
+        .catch(error => {
+          console.log('SW reg failed', error);
+        })
     }
-  })
+  });
 
